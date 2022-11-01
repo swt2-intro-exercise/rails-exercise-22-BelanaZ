ac5453193a56adf0bd230e17468ff16c7410a46c
BelanaZ <66524915+BelanaZ@users.noreply.github.com>
2022-11-01 17:32:49 +0100

    add validation for input #7

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,32 +1,32 @@
-    describe "New author page", type: :model do
-        it "should create a new instance" do
-            first_name = 'Alan'
-            last_name = 'Turing'
-            homepage = 'http://wikipedia.org/Alan_Turing'
-            author = Author.new(:first_name => first_name, :last_name => last_name, :homepage => homepage)
-            expect(author.first_name).to eq(first_name)
-            expect(author.last_name).to eq(last_name)
-            expect(author.homepage).to eq(homepage)
-        end
-
-        it "should return a name" do
-            first_name = 'Alan'
-            last_name = 'Turing'
-            homepage = 'http://wikipedia.org/Alan_Turing'
-            author = Author.new(:first_name => first_name, :last_name => last_name, :homepage => homepage)
-            expect(author.name).to eq("#{first_name} #{last_name}")
-        end
-
-        it "should validate author with a last name that is not empty" do
-            @alan = FactoryBot.create :author
-            expect(@alan).to be_valid
-        end
-
-        it "should not validate author with a last name that is not empty"do
-            first_name = 'Elton'
-            last_name = ''
-            homepage = 'http://wikipedia.org/Elton'
-            author = Author.new(:first_name => first_name, :last_name => last_name, :homepage => homepage)
-            expect(@author).to_not be_valid
-        end
+describe "New author page", type: :model do
+    it "should create a new instance" do
+        first_name = 'Alan'
+        last_name = 'Turing'
+        homepage = 'http://wikipedia.org/Alan_Turing'
+        author = Author.new(:first_name => first_name, :last_name => last_name, :homepage => homepage)
+        expect(author.first_name).to eq(first_name)
+        expect(author.last_name).to eq(last_name)
+        expect(author.homepage).to eq(homepage)
     end
+
+    it "should return a name" do
+        first_name = 'Alan'
+        last_name = 'Turing'
+        homepage = 'http://wikipedia.org/Alan_Turing'
+        author = Author.new(:first_name => first_name, :last_name => last_name, :homepage => homepage)
+        expect(author.name).to eq("#{first_name} #{last_name}")
+    end
+
+    it "should validate author with a last name that is not empty" do
+        @alan = FactoryBot.create :author
+        expect(@alan).to be_valid
+    end
+
+    it "should not validate author with a last name that is not empty"do
+        first_name = 'Elton'
+        last_name = ''
+        homepage = 'http://wikipedia.org/Elton'
+        author = Author.new(:first_name => first_name, :last_name => last_name, :homepage => homepage)
+        expect(author).to_not be_valid
+    end
+end
