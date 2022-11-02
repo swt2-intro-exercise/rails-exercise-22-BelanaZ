53284d6296d0c391ebd0e98acab54bfea496af2b
BelanaZ <66524915+BelanaZ@users.noreply.github.com>
2022-11-02 16:00:05 +0100

    add paper edit link & basic test #14

--- spec/views/papers/edit.html.erb_spec.rb
+++ spec/views/papers/edit.html.erb_spec.rb
@@ -25,4 +25,32 @@ RSpec.describe "papers/edit", type: :view do
       assert_select "input[name=?]", "paper[year]"
     end
   end
+
+  it "should exist at 'edit_paper_path' and render withour error" do
+    visit edit_paper_path(paper)
+ end
+
+ it "should have text inputs for an papers's title, venue and year" do
+    visit edit_paper_path(paper)
+    expect(page).to have_field('paper[title]')
+    expect(page).to have_field('paper[venue]')
+    expect(page).to have_field('paper[year]')
+  end
+
+  it "should save the edited paper when data is submitted" do
+    visit edit_paper_path(paper)
+    page.fill_in 'paper[title]', with: 'a Title'
+    page.fill_in 'paper[venue]', with: 'venue'
+    page.fill_in 'paper[year]', with: '1200'
+    find('input[type="submit"]').click
+  end
+
+  it "should update the information in the database" do
+    visit edit_paper_path(paper)
+      title = "new title"
+      page.fill_in 'paper[title]', with: title
+      find('input[type="submit"]').click
+      paper.reload
+      expect(paper.title).to eq(title)
+  end
 end
