3b9d86e8d41f4c11ec983f9413961e64c7512785
BelanaZ <66524915+BelanaZ@users.noreply.github.com>
2022-11-02 15:09:23 +0100

    failing test for #13

--- spec/views/papers/new.html.erb_spec.rb
+++ spec/views/papers/new.html.erb_spec.rb
@@ -26,11 +26,38 @@ RSpec.describe "papers/new", type: :view do
     end
   end
 
-  it "should display 'error' after invalid input" do
+  it "should display 'error' after invalid title" do
     visit new_paper_path
     page.fill_in 'paper[title]', with: ''
     page.fill_in 'paper[venue]', with: 'Paris'
     page.fill_in 'paper[year]', with: '1999'
+    find('input[type="submit"]').click
+    expect(page).to have_text 'error'
+  end
+
+  it "should display 'error' after invalid venue" do
+    visit new_paper_path
+    page.fill_in 'paper[title]', with: 'Why Squeak is not the best programming language ever'
+    page.fill_in 'paper[venue]', with: ''
+    page.fill_in 'paper[year]', with: '1999'
+    find('input[type="submit"]').click
+    expect(page).to have_text 'error'
+  end
+
+  it "should display 'error' after invalid, empty year" do
+    visit new_paper_path
+    page.fill_in 'paper[title]', with: 'How to train a dragon'
+    page.fill_in 'paper[venue]', with: 'Home'
+    page.fill_in 'paper[year]', with: ''
+    find('input[type="submit"]').click
+    expect(page).to have_text 'error'
+  end
+
+  it "should display 'error' after invalid, non-numeric year" do
+    visit new_paper_path
+    page.fill_in 'paper[title]', with: 'Finishing your assignment a week before it is due'
+    page.fill_in 'paper[venue]', with: 'Uni'
+    page.fill_in 'paper[year]', with: 'Never'
     find('input[type="submit"]').click
     expect(page).to have_text 'error'
   end
