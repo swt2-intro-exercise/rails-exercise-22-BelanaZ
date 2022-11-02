ea182b02806459e19c325a12dae9a2706d53ea0e
BelanaZ <66524915+BelanaZ@users.noreply.github.com>
2022-11-02 13:39:46 +0100

    failing test for #10

--- spec/features/author/show_page_spec.rb
+++ spec/features/author/show_page_spec.rb
@@ -13,4 +13,23 @@ describe "Showing author page", type: :feature do
         expect(page).to have_text('Turing')
         expect(page).to have_text('http://wikipedia.de/alan_turing')
     end
+
+    it "has delete button" do
+        @alan = FactoryBot.create :author
+        visit authors_path(@alan)
+        expect(page).to have_link('Delete')
+    end
+
+    it "allows deleting the author" do
+        @author = Author.new(
+            first_name: 'Alan',
+            last_name: 'Turing',
+            homepage: 'http://wikipedia.org/Alan_Turing'
+        )
+        @author.save
+        visit authors_path(@author)
+        count_before = Author.count
+        click_link("Delete")
+        expect(Author.count).to eq count_before -1  
+    end
  end
