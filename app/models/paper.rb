f782b17571a93c06842a8a29a3e585c80c61b108
BelanaZ <66524915+BelanaZ@users.noreply.github.com>
2022-11-07 13:57:44 +0100

    allow filtering of papers by year #19

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -3,4 +3,5 @@ class Paper < ApplicationRecord
     validates :title, presence: true, length: { minimum: 1 }
     validates :venue, presence: true, length: {minimum: 1}
     validates :year, presence: true, length: {minimum: 1}, numericality: { only_integer: true }
+    scope :year, ->(year) { where("year == ?", year) if year.present?}
 end
